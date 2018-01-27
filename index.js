9d3e8a6c20c747633add475f3d1f68d3709d83e3
Tim Cross <theophilusx@gmail.com>
2018-01-28 10:20:40 +1100

    Update to verify data

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ where <file> is the path to an NetCDF input file
 }
 
 const fs = require('fs');
+const {awapInit} = require("./awap");
 
 var inFile = process.argv[2];
 
@@ -28,26 +29,49 @@ console.log(`Processing nc file ${inFile}`);
 var bn = require('./bombadier-netcdf');
 
 var nc = bn.netcdfInit(inFile);
+var validTime = nc.validTimes;
+var latitudes = nc.getLatitudes();
+var longitudes = nc.getLongitudes();
+var awapGrid = awapInit(latitudes, longitudes);
 
 console.log(nc.metaInfo());
 
-console.log("Raw Times: ", nc.rawTimes);
+console.log(`AWAP GRID: ${awapGrid.length} x ${awapGrid[0].length}`);
 
-console.log("Valid Times: ", nc.validTimes);
+// for (let lat = 0; lat < awapGrid.length; lat++) {
+//   for (let lon = 0; lon < awapGrid[lat].length; lon++) {
+//     if (awapGrid[lat][lon] !== undefined) {
+//       console.log(`Lat: ${lat} Lon ${lon} AWAP: ${awapGrid[lat][lon]}`);
+//     }
+//   }
+// }
 
 var grid = nc.readGrid(0);
 
 console.log("Grid Size: " + grid.length + " by " + grid[0].length);
 
-// for (let i = 0; i < grid.length; i++) {
-//   for (let j = 0; j < grid[i].length; j++) {
-//     if (!isNaN(grid[i][j])) {
-//       console.log(`lat: ${i} lon: ${j} = ${roundValue(2, grid[i][j])}`);
-//     }
-//   }
-// }
+var results = [];
 
+for (let lat = 0; lat < awapGrid.length; lat++) {
+  for (let lon = 0; lon < awapGrid[lat].length; lon++) {
+    let awapId = awapGrid[lat][lon];
+    if (awapId !== undefined) {
+      results.push({
+        latIdx : lat,
+        lonIdx : lon,
+        latitude : latitudes[lat],
+        longitude : longitudes[lon],
+        awapId : awapId,
+        pr : grid[lat][lon]
+      });
+    }
+  }
+}
 
+for (let r of results) {
+  console.log(r);
+}
 
+console.log("Number of results: ", results.length);
 
 nc.close();
